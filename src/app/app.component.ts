02af71d0221d65db6de0156865e97b3e40e85508
Nathan Brent Taylor <NathanTaylorBiz@gmail.com>
2022-05-14 17:10:22 -0500

    Updating title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GenZero';
+
+  constructor (
+    readonly router: Router,
+    readonly titleSrv: Title,
+    readonly data: DataService
+  ) {}
+
+  ngOnInit(): void {
+    this.titleSrv.setTitle("GenZero - Skill Calculator");
+  }
 }
