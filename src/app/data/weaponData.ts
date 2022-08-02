d50f7ae3c66849338326173b33a77db687eebbbb
Nathan Brent Taylor <NathanTaylorBiz@gmail.com>
2022-08-02 12:22:07 -0500

    Adding More weapon svgs

--- src/app/data/weaponData.ts
+++ src/app/data/weaponData.ts
@@ -1,4 +1,19 @@
 export class WeaponWheel {
-    crown: number;
     slot: number;
+    id?: string;
+    crown?: number;
+    ammoTypes?: string[];
+    vision?: string[];
+    barrel?: {
+        id?: string;
+        crown?: number;
+    };
+    scope?: {
+        id?: string;
+        crown?: number;
+    }
+    magazine?: {
+        id?: string;
+        crown?: number;
+    };
 }
